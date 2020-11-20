ccf50835a1f04a073e576c4c71378c555af12456
Navnit Kumar <navnitkumar1224@gmail.com>
2020-11-20 13:25:35 +0530

    Create leaders.cpp

---
+++ Must Do Coding/Arrays/leaders.cpp
@@ -0,0 +1,23 @@
+// O(n^2)
+void printLeaders(vector<int> arr, int n){
+    int i,j;
+    for(i=0; i<n; i++){
+        for(j=i+1; j<n; j++){
+            if(arr[i]<arr[j])
+                break;
+        }
+        if(j == n)  cout<<arr[i]<<" ";
+    }
+}
+
+// O(n)
+void printLeaders(vector<int> arr, int n){
+    int i, maxFromRight = arr[n-1];
+    cout<<maxFromRight<<" ";
+    for(int i= n-2; i>=0; i--){
+        if(arr[i] >= maxFromRight){
+            maxFromRight = arr[i];
+            cout<<maxFromRight<<" ";
+        }
+    }
+} 
