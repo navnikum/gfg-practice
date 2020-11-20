6f07e8ad61b5f657084b7deb9f641f55c86ffe39
Navnit Kumar <navnitkumar1224@gmail.com>
2020-11-20 22:27:32 +0530

    Update Remove All Adjacent Duplicates In String.cpp

--- Strings/Remove All Adjacent Duplicates In String.cpp
+++ Strings/Remove All Adjacent Duplicates In String.cpp
@@ -32,3 +32,30 @@ string removeDuplicates(string S) {
             return removeDuplicates(S.substr(0, i-1)+S.substr(i+1));
     return S;
 }
+
+
+
+// Input : aaaaaaaaaaaaa -> Output : _
+void removeDuplicates(string& str) {
+	char prev = str[0];	// stores previous valid character
+	int k = 0;  // k maintains the index of last filled location in the result
+	int i = 1;  // start from second character
+
+	while (i < str.length()){
+		// if the current character is not same as the previous character
+		if (prev != str[i]){
+			str[++k] = str[i++];	// update result
+			prev = str[k];  		// update prev
+		}
+		else{
+			// remove adjacent duplicates
+			while (prev == str[i])
+				i++;
+
+			prev = str[--k];		// update prev
+		}
+	}
+
+	// delete str[k+1, n) as result lies in str[0..k]
+	str.erase(k + 1);
+}
