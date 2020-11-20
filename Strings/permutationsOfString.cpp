46bd3577db2a71372980e50b4684483bdb9009c3
Navnit Kumar <navnitkumar1224@gmail.com>
2020-11-20 20:44:51 +0530

    Update permutationsOfString.cpp

--- Strings/permutationsOfString.cpp
+++ Strings/permutationsOfString.cpp
@@ -1,14 +1,31 @@
 // backtracking O(n*n!)
+#include <bits/stdc++.h>
+using namespace std;
+set<string> ans;
 void permutations(string s, int l, int r){
     if(l == r){
-        ans.push_back(s);
+        ans.insert(s);
         return;
     }
     else{
         for(int i=l; i<=r; i++){
-            swap(s[l], s[i]);
+            swap(s[i],s[l]);
             permutations(s, l+1, r);
-            swap(s[l], s[i]);
+            swap(s[i],s[l]);
         }
     }
+}
+int main() {
+	int t; cin>>t;
+	while(t--){
+	    ans.clear();
+	    string s;
+	    cin>>s;
+	    int k=s.size();
+	    permutations(s, 0, k-1);
+	    for(auto it: ans)
+	        cout<<it<<" ";
+	    cout<<endl;
+	}
+	return 0;
 }
