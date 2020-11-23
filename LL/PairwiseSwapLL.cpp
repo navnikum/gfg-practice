ee1a36ad7ed6403dae9c19e1b69cf531d3a50f41
Navnit Kumar <navnitkumar1224@gmail.com>
2020-11-23 12:39:10 +0530

    Create PairwiseSwapLL.cpp

---
+++ LL/PairwiseSwapLL.cpp
@@ -0,0 +1,35 @@
+Node* pairWiseSwap(Node* head){
+    if (head == NULL || head->next == NULL) 
+        return head; 
+    // swap first two
+    Node *curr, *prev;
+    prev = head; 
+    curr = head->next->next;
+    // reverse link
+    head->next->next = head;
+    // fix head
+    head = head->next;
+    
+    while(curr && curr->next){
+        prev->next = curr->next;
+        prev = curr;
+        
+        Node *next = curr->next->next;
+        curr->next->next = curr;
+        
+        curr = next;
+    }
+    
+    prev->next = curr;
+    return head;
+}
+// Recursive
+Node* pairWiseSwap(Node* head){
+    if (head == NULL || head->next == NULL) 
+        return head; 
+    Node* curr = head->next->next;
+    head->next->next = head;
+    Node* newhead = head->next;
+    head->next = pairWiseSwap(curr);
+    return newhead;
+}
