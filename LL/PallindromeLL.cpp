f21edd94a71ee276205787d8f8dcf799cb7a690a
Navnit Kumar <navnitkumar1224@gmail.com>
2020-12-03 13:28:20 +0530

    Update PallindromeLL.cpp

--- LL/PallindromeLL.cpp
+++ LL/PallindromeLL.cpp
@@ -11,6 +11,27 @@ bool isPalindrome(Node* head){
 	return 1;
 }
 
+int Solution::lPalin(ListNode* A) {
+    ListNode *slow = A, *fast = A;
+    
+    while(fast && fast->next){
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    //  1 2 3 5 4 6 7 // odd : slow = mid_elem ; fast = end
+    //  1 2 3 5 4 6   // even: slow = mid+1 ; fast = null 
+    slow = reverse(slow);
+    fast = A;
+    while(fast && slow){
+        if(fast->val != slow->val)
+            return 0;
+        fast = fast->next;
+        slow = slow->next;
+    }
+    return 1;
+}
+
+
 // Recursion
 bool util(Node** l, Node* r){
     if(r==NULL) return true;
@@ -22,58 +43,4 @@ bool util(Node** l, Node* r){
 }
 bool isPalindrome(Node *head){
     return util(&head, head);
-}
-
-// Pallindrome LL find mid and reverse
-// O(n) 
-void reverse(Node** head_ref){ 
-	Node* prev = NULL, current = *head_ref, next; 
-	while (current) { 
-		next = current->next; 
-		current->next = prev; 
-		prev = current; 
-		current = next; 
-	} 
-	*head_ref = prev; 
-}
-bool compareLists(struct Node* head1, struct Node* head2){ 
-  	if (!head1 && !head2)	return 1; 
-	Node* temp1 = head1, temp2 = head2; 
- 
-	while (temp1 && temp2) { 
-		if (temp1->data != temp2->data) 
-        	return 0;  
-		temp1 = temp1->next; 
-		temp2 = temp2->next; 
-	} 
-	return 1; 
-}
-bool isPalindrome(Node *head) {
-    bool res = true;
-    Node *slow, *fast, *prev_slow = NULL, *midNode = NULL, *secondHalf;
-    slow = fast = head;
-    if(head && head->next){
-        while(fast && fast->next){
-            fast = fast->next->next;
-            prev_slow = slow;
-            slow = slow->next;
-        }
-        if(fast != NULL){ // odd list store middle node somewhere
-            midNode = slow;
-            slow = slow->next;
-        }
-        secondHalf = slow;
-        prev_slow->next = NULL;
-        reverse(&secondHalf);
-        res = compareLists(head, secondHalf);
-        reverse(&secondHalf);
-        // odd list
-        if(midNode != NULL){
-            prev_slow->next = midNode;
-            midNode->next = secondHalf;
-        }
-        else    // even list
-            prev_slow->next = secondHalf;
-    }
-    return res;
 }
