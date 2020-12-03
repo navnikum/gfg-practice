28dd496644e8a67958c45af9ca0f39cc69cdd2ee
Navnit Kumar <navnitkumar1224@gmail.com>
2020-12-03 13:21:09 +0530

    Create rotateLL_new.cpp

---
+++ LL/rotateLL_new.cpp
@@ -0,0 +1,21 @@
+ListNode* Solution::rotateRight(ListNode* A, int B) {
+    ListNode *fp=A,*sp=A;
+    int count = 0;
+    while(count<B){
+        fp = fp->next;
+        if(fp==NULL)
+            fp = A;
+        count++;
+    }
+    if(fp==NULL || fp == sp)
+        return A;
+    while(fp && fp->next){
+        fp=fp->next;
+        sp=sp->next;
+    }
+    ListNode *head = sp->next;
+    sp->next = NULL;
+    fp->next = A;
+    return head;
+    
+}
